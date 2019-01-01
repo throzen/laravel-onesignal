5c7e352817b989e17111f85eb795cf70f6e27016
Murat Demir <muratdemir@teknasyon.com>
2019-01-01 21:25:53 +0300

    api url added to config

--- config/onesignal.php
+++ config/onesignal.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'app_id' => 'YOUR-APP-ID',
-    'rest_api_key' => 'YOUR-REST-API-KEY',
-    'user_auth_key' => 'YOUR-USER-AUTH-KEY'
+    'api_url ' => env('ONE_SIGNAL_API_URL'),
+
+    'app_id' => env('ONE_SIGNAL_APP_ID'),
+    'rest_api_key' => env('ONE_SIGNAL_API_KEY'),
+    'user_auth_key' => env('ONE_SIGNAL_AUTH_KEY')
 ];
