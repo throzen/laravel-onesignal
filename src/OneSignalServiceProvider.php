045ecf510f96cffbbaffeba5b89eb600a6f0f92e
Murat Demir <muratdemir@teknasyon.com>
2019-01-01 16:32:48 +0300

    dirname added config path.

--- src/OneSignalServiceProvider.php
+++ src/OneSignalServiceProvider.php
@@ -4,8 +4,16 @@ namespace MuratDemir\OneSignal;
 
 use Illuminate\Support\ServiceProvider;
 
-class OneSignalProvider extends ServiceProvider
+class OneSignalServiceProvider extends ServiceProvider
 {
+
+
+    public function boot()
+    {
+        $this->publishes([
+            dirname(__DIR__) . '/config/onesignal.php' => config_path('onesignal.php'),
+        ]);
+    }
 
     public function register()
     {
@@ -13,13 +21,6 @@ class OneSignalProvider extends ServiceProvider
             dirname(__DIR__) . '/config/onesignal.php', 'onesignal'
         );
 
-    }
-
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__ . '/config/onesignal.php' => config_path('onesignal.php'),
-        ]);
     }
 
 }
