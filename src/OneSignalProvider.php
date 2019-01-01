983b45bc145d5e1ac06705da7971747507ee51f1
Murat Demir <muratdemir@teknasyon.com>
2019-01-01 16:19:42 +0300

    merged config files.

--- src/OneSignalProvider.php
+++ src/OneSignalProvider.php
@@ -7,4 +7,19 @@ use Illuminate\Support\ServiceProvider;
 class OneSignalProvider extends ServiceProvider
 {
 
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/config/onesignal.php', 'onesignal'
+        );
+
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/onesignal.php' => config_path('onesignal.php'),
+        ]);
+    }
+
 }
